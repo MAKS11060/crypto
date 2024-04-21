8c8d940b491827106464282e9111644ecfad73fc
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2024-04-21 16:28:36 +0200

    add example, exclude .github

--- src/keys.ts
+++ src/keys.ts
@@ -1,5 +1,15 @@
 import {keyAlg, type KeyAlg} from './jwk.ts'
 
+/**
+ * Generate key pair.
+ *
+ * @example
+ * ```ts
+ * import {generateKeyPair} from '@maks11060/crypto'
+ *
+ * const keys = await generateKeyPair('Ed25519')
+ * ```
+ */
 export const generateKeyPair = async (alg: KeyAlg): Promise<CryptoKeyPair> => {
   const keys = await crypto.subtle.generateKey(keyAlg(alg), true, [
     'sign',
